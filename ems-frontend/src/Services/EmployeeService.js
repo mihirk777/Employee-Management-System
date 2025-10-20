a66b2b1d4bcd813f1a8453d2f7510a57dbcc0432
mihir <mihirkashid777@gmail.com>
2025-10-21 04:43:06 +0530

    Connect React App to Update Employee REST API

--- ems-frontend/src/Services/EmployeeService.js
+++ ems-frontend/src/Services/EmployeeService.js
@@ -5,3 +5,4 @@ const REST_API_BASE_URL='http://localhost:8080/api/employees';
 export const listEmployee=()=> axios.get(REST_API_BASE_URL);
 export const createEmployee=(employee)=>axios.post(REST_API_BASE_URL,employee);
 export const getEmployee=(employeeId)=>axios.get(REST_API_BASE_URL+'/'+employeeId)
+export const updateEmployee=(employeeId,employee)=>axios.put(REST_API_BASE_URL+'/'+employeeId,employee)
