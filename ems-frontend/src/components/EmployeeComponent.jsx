c8e1c558806e6aacbdacbc2e01514fb4f72b17c3
mihir <mihirkashid777@gmail.com>
2025-10-21 03:04:06 +0530

    Connect React App to Add Employee REST API

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { createEmployee } from '../Services/EmployeeService';
 
 const EmployeeComponent = () => {
     const [firstName,setFirstName]=useState("");
     const [lastName,setLastName]=useState("");
     const[email,setEmail]=useState("");
 
+    const navigator=useNavigate();
     function saveEmployee(e){
         e.preventDefault();
         const employee={firstName,lastName,email};
         console.log(employee);
+        createEmployee(employee).then((res)=>{
+            console.log(res.data);
+            navigator('/employees')
+             
+        })
     }
   return (
 
