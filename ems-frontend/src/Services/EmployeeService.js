ffa021434f3b936d0c383471e43247955473f6a9
mihir <mihirkashid777@gmail.com>
2025-10-26 03:34:57 +0530

    dockerize the application

--- ems-frontend/src/Services/EmployeeService.js
+++ ems-frontend/src/Services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL='http://localhost:8080/api/employees';
+const REST_API_BASE_URL=process.env.BASE_URL;
 
 export const listEmployee=()=> axios.get(REST_API_BASE_URL);
 export const createEmployee=(employee)=>axios.post(REST_API_BASE_URL,employee);
